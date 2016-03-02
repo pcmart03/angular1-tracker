ec23cdf0398002b006a317a1f65414cf9794792d
Patrick Martin <pcmart03@gmail.com>
2016-03-01 22:39:39 -0500

    styling and begining work on nutrionix api

---
+++ app/app.js
@@ -0,0 +1,1 @@
+var nutritionApp = angular.module('nutritionApp', ['ngResource']);
---
+++ app/controllers/food-log.js
@@ -0,0 +1,22 @@
+nutritionApp.controller('FoodLogController', ['$scope', function($scope){
+    $scope.foodsEaten = {
+        foods: [
+            {
+                name: "Tacos",
+                calories: 400
+            },
+            {
+                name: "hot dogs",
+                calories: 120.5
+            },
+            {
+                name: "Pie",
+                calories: 700
+            },
+            {
+                name: "apple",
+                calories: 87.3
+            }
+        ]
+    };
+}]);
